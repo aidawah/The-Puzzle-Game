87cfa1e60ef6b769965e6daa9d9b48270280579d
Aiden Wahed <aiden@healthspaces.com>
2025-08-18 14:23:53 -0600

    updating svelte kit

--- svelte.config.js
+++ svelte.config.js
@@ -4,14 +4,18 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
   preprocess: vitePreprocess(),
 
-  kit: {
-    adapter: adapter({
-      // where to write the built files:
-      pages: 'build',
-      assets: 'build',
-      // any 404 should serve this:
-      fallback: 'index.html'
-    })
+  // kit: {
+  //   adapter: adapter({
+  //     // where to write the built files:
+  //     pages: 'build',
+  //     assets: 'build',
+  //     // any 404 should serve this:
+  //     fallback: 'index.html'
+  //   })
+
+    kit: {
+    adapter: adapter({ pages: 'build', assets: 'build', fallback: 'index.html' }),
+    prerender: { entries: [] }
 
   }
 };
