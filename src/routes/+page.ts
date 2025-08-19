3cc229ba5f972c979356cf4d292eaa32cf2a32f4
Aiden Wahed <aiden@healthspaces.com>
2025-08-19 16:57:16 -0600

    updating homepage randomization

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -5,7 +5,14 @@ import { fetchPuzzles } from '$lib/firebase';
 export const load: PageLoad = async () => {
 	const puzzles = await fetchPuzzles();
 	// choose the first (or apply logic for selecting)
-	return {
-		puzzle: puzzles[0] ?? null
-	};
+	// return {
+	// 	puzzle: puzzles[0] ?? null
+	// };
+
+	const puzzle = puzzles.length ? puzzles[Math.floor(Math.random() * puzzles.length)] : null
+
+	console.log('puzzle obj', puzzle)
+
+	return {puzzle}
+
 };
